c99e4906682eb837fe970db3a1c8e139dad73f1f
Sarah Wells <sarah.wells@ft.com>
2015-01-20 12:22:48 +0000

    Reformatted so can easily compare the before and after cases rather than having to scroll

--- src/test/java/com/ft/bodyprocessing/xml/eventhandlers/EndToEndStaxHandlerTest.java
+++ src/test/java/com/ft/bodyprocessing/xml/eventhandlers/EndToEndStaxHandlerTest.java
@@ -30,9 +30,16 @@ public class EndToEndStaxHandlerTest {
         registry.registerDefaultEventHandler(new StripXMLEventHandler());
         registry.registerStartAndEndElementEventHandler(handler,"blockquote");
 
-        String doc = "<div data-asset-type=\"embed\"><blockquote class=\"twitter-tweet\" lang=\"en\"><p>Fascinating chart - perceptions of female &amp; male leadership advantages<a href=\"http://t.co/VYrmvvaPIl\">http://t.co/VYrmvvaPIl</a> <a href=\"http://t.co/iyrtPRDuJN\">pic.twitter.com/iyrtPRDuJN</a></p>— Conrad Hackett (@conradhackett) <a href=\"https://twitter.com/conradhackett/status/555409369374801920\">January 14, 2015</a></blockquote>\n<script async=\"async\" charset=\"utf-8\" src=\"//platform.twitter.com/widgets.js\"/></div>";
+        String doc = "<div data-asset-type=\"embed\"><blockquote class=\"twitter-tweet\" lang=\"en\">" +
+        		"<p>Fascinating chart - perceptions of female &amp; male leadership advantages<a href=\"http://t.co/VYrmvvaPIl\">http://t.co/VYrmvvaPIl</a> " +
+        		"<a href=\"http://t.co/iyrtPRDuJN\">pic.twitter.com/iyrtPRDuJN</a></p>— Conrad Hackett (@conradhackett) " +
+        		"<a href=\"https://twitter.com/conradhackett/status/555409369374801920\">January 14, 2015</a></blockquote>\n" +
+        		"<script async=\"async\" charset=\"utf-8\" src=\"//platform.twitter.com/widgets.js\"/></div>";
 
-        String expectedDoc = "<blockquote class=\"twitter-tweet\" lang=\"en\"><p>Fascinating chart - perceptions of female &amp; male leadership advantages<a href=\"http://t.co/VYrmvvaPIl\">http://t.co/VYrmvvaPIl</a> <a href=\"http://t.co/iyrtPRDuJN\">pic.twitter.com/iyrtPRDuJN</a></p>— Conrad Hackett (@conradhackett) <a href=\"https://twitter.com/conradhackett/status/555409369374801920\">January 14, 2015</a></blockquote>";
+        String expectedDoc = "<blockquote class=\"twitter-tweet\" lang=\"en\">" +
+        		"<p>Fascinating chart - perceptions of female &amp; male leadership advantages<a href=\"http://t.co/VYrmvvaPIl\">http://t.co/VYrmvvaPIl</a> " +
+        		"<a href=\"http://t.co/iyrtPRDuJN\">pic.twitter.com/iyrtPRDuJN</a></p>— Conrad Hackett (@conradhackett) " +
+        		"<a href=\"https://twitter.com/conradhackett/status/555409369374801920\">January 14, 2015</a></blockquote>";
 
 
         String result = processor.process(doc, TestBodyProcessingContext.testContext());
@@ -49,9 +56,18 @@ public class EndToEndStaxHandlerTest {
         registry.registerDefaultEventHandler(new StripXMLEventHandler());
         registry.registerStartAndEndElementEventHandler(handler,"blockquote");
 
-        String doc = "<div data-asset-type=\"embed\"><blockquote class=\"twitter-tweet\" lang=\"en\"><p>Learning from Comcast/TWC? AT&amp;T b DirecTV deal includes collar protecting <a href=\"https://twitter.com/search?q=%24DTV&amp;src=ctag\">$DTV</a> shareholders from decline in <a href=\"https://twitter.com/search?q=%24T&amp;src=ctag\">$T</a> stock. (Caps upside, too).</p>&mdash; Liz Hoffman (@lizrhoffman) <a href=\"https://twitter.com/lizrhoffman/statuses/468146880682016769\">May 18, 2014</a></blockquote><script src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script></div>";
+        String doc = "<div data-asset-type=\"embed\"><blockquote class=\"twitter-tweet\" lang=\"en\">" +
+        		"<p>Learning from Comcast/TWC? AT&amp;T b DirecTV deal includes collar protecting " +
+        		"<a href=\"https://twitter.com/search?q=%24DTV&amp;src=ctag\">$DTV</a> shareholders from decline in " +
+        		"<a href=\"https://twitter.com/search?q=%24T&amp;src=ctag\">$T</a> stock. (Caps upside, too).</p>&mdash; Liz Hoffman (@lizrhoffman) " +
+        		"<a href=\"https://twitter.com/lizrhoffman/statuses/468146880682016769\">May 18, 2014</a></blockquote>" +
+        		"<script src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script></div>";
 
-        String expectedDoc = "<blockquote class=\"twitter-tweet\" lang=\"en\"><p>Learning from Comcast/TWC? AT&amp;T b DirecTV deal includes collar protecting <a href=\"https://twitter.com/search?q=%24DTV&amp;src=ctag\">$DTV</a> shareholders from decline in <a href=\"https://twitter.com/search?q=%24T&amp;src=ctag\">$T</a> stock. (Caps upside, too).</p>&mdash; Liz Hoffman (@lizrhoffman) <a href=\"https://twitter.com/lizrhoffman/statuses/468146880682016769\">May 18, 2014</a></blockquote>";
+        String expectedDoc = "<blockquote class=\"twitter-tweet\" lang=\"en\">" +
+        		"<p>Learning from Comcast/TWC? AT&amp;T b DirecTV deal includes collar protecting " +
+        		"<a href=\"https://twitter.com/search?q=%24DTV&amp;src=ctag\">$DTV</a> shareholders from decline in " +
+        		"<a href=\"https://twitter.com/search?q=%24T&amp;src=ctag\">$T</a> stock. (Caps upside, too).</p>&mdash; Liz Hoffman (@lizrhoffman) " +
+        		"<a href=\"https://twitter.com/lizrhoffman/statuses/468146880682016769\">May 18, 2014</a></blockquote>";
 
 
         String result = processor.process(doc, TestBodyProcessingContext.testContext());
