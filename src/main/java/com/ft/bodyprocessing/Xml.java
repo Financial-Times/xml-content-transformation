1c5c31c8f44cd400ba62ddbb0251ea4d234feabc
Simon.Gibbs <simon.gibbs@ft.com>
2015-03-25 11:29:58 +0000

    Add a utility for reading XML as a DOM

--- src/main/java/com/ft/bodyprocessing/Xml.java
+++ src/main/java/com/ft/bodyprocessing/Xml.java
@@ -1,13 +1,20 @@
 package com.ft.bodyprocessing;
 
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 
 /**
@@ -31,4 +38,16 @@ public class Xml {
 		}
 	}
 
+    public static Document createDocument(String bodyHtml) {
+        try {
+
+            InputSource is = new InputSource();
+            is.setCharacterStream(new StringReader(bodyHtml));
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
+
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            throw new BodyProcessingException(e);
+        }
+
+    }
 }
